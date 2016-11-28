30e8fc543c580629994bb9035609adee3fd0b921
Alexander Solovov <a_solovov@yahoo.com>
2016-11-29 03:03:49 +0600

    Map generation function has been adjusted to better zoom on the main parcel.

---
+++ changeset/1611a.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO system.version SELECT '1611a' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1611a');
+
+INSERT INTO system.setting(name, vl, active, description) VALUES ('form23_report_url', '/reports/cert/Form23', 't', 'Path to form23 report');
